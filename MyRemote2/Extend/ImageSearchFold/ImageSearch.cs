d81787f7438380291ccdfe97442ca651362a0b1d
scriptmfc <scriptmfc@gmail.com>
2023-06-15 18:25:54 +0900

    _

---
+++ MyRemote2/Extend/ImageSearchFold/ImageSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRemote2.Extend.ImageSearchFold
+{
+    public static class ImageSearch
+    {
+        public static Bitmap TargetBitmap;
+
+
+
+    }
+}
---
+++ MyRemote2/Extend/ImageSearchFold/LocationSetting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRemote2.Extend.ImageSearchFold
+{
+    public static class LocationSetting
+    {
+        public static int StartX;
+        public static int StartY;
+        public static int EndX;
+        public static int EndY;
+
+        public static Step currentStep;
+        public enum Step
+        {
+            None,
+            StartPointSetting,
+            EndPointSetting
+        }
+
+        public static void LocationPointSettingStart()
+        {
+            currentStep = Step.StartPointSetting;
+        }
+
+        public static void LocationPointSetting_StartPoint()
+        {
+            var point = WindowRobotFold.WorkFold.MouseWork.GetMouseCurrentPosition();
+
+            StartX = (int)point.X;
+            StartY = (int)point.Y;
+        }
+        public static void LocationPointSetting_EndPoint()
+        {
+            var point = WindowRobotFold.WorkFold.MouseWork.GetMouseCurrentPosition();
+
+            EndX = (int)point.X;
+            EndY = (int)point.Y;
+        }
+
+
+
+
+    }
+}
